e8f88ea1f418aa999524cf23d45a09055e6e9fbb
Gayanika <gayanikaishani25@gmail.com>
2025-02-24 21:34:46 +0530

    Add files via upload

---
+++ Area.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    double length = 25.12428;
+    double width = 12.59;
+    double area = length * width;
+
+    printf("Area of the rectangle: %.4f cm^2\n", area);
+
+    return 0;
+}
---
+++ XML.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf(" The Reverse of XML is %c %c %c\n", 'L', 'M', 'X');
+    return 0;
+}
